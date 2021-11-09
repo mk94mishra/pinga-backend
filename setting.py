54df662f96e0b407a524a14260b6d153525a9cc6
Manish Mishra <mk94mishra@gmail.com>
2021-11-09 16:40:25 +0530

    signup

--- setting.py
+++ setting.py
@@ -8,5 +8,5 @@ from databases import Database
 database = Database('postgresql://{}:{}@{}:{}/{}'.format(config['db_user'],config['db_password'],config['db_host'],config['db_port'],config['db_name']))
 
 #3 public endpoints
-endpoint_public=[("GET",""),("GET","docs"),("GET","redoc"),("GET","openapi.json"),("POST","user/login-admin"),("POST","user/login-non-admin")]
+endpoint_public=[("GET",""),("GET","docs"),("GET","redoc"),("GET","openapi.json"),("POST","user/login-admin"),("POST","user/login-non-admin"),("POST","user/signup-normal")]
 
