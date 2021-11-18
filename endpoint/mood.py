c60d4d5a4b51c494d9df3cc5d422c6d6fc19f220
Manish Mishra <mk94mishra@gmail.com>
2021-11-18 12:50:51 +0530

    mood

--- endpoint/mood.py
+++ endpoint/mood.py
@@ -22,7 +22,7 @@ async def mood_create(request:Request,payload:mood):
    payload=json.dumps(payload.dict()) 
    today=date.today()
    #mood get today of self user
-   query="""select * from mood where created_by_id=:created_by_id and created_at::date=:today"""
+   query="""select * from mood where created_by_id=:created_by_id and created_at::date=:today and type='mood'"""
    values={"created_by_id":user_id,"today":today}
    response=await database_fetch_all(query,values)
    if response["status"]=="false":
@@ -32,8 +32,8 @@ async def mood_create(request:Request,payload:mood):
    mood=row
    #mood not exist
    if mood==[]:
-      query="""insert into mood (created_by_id,data) values (:created_by_id,json_build_object('moodtype',:payload))"""
-      values={"created_by_id":user_id, "payload":payload}
+      query="""insert into mood (created_by_id,type,data) values (:created_by_id,:type,json_build_object('moodtype',:payload))"""
+      values={"created_by_id":user_id,"type":'mood', "payload":payload}
       response=await database_execute(query,values)
       if response["status"]=="false":
          raise HTTPException(status_code=400,detail=response)
@@ -49,7 +49,44 @@ async def mood_create(request:Request,payload:mood):
    #finally
    return response
 
-    
+
+#1 mood:sex o scale create
+@router.post("/sex-o-scale")
+async def sex_o_scale_create(request:Request,payload:mood):
+   #prework
+   user_id = request.state.user_id
+   payload=json.dumps(payload.dict()) 
+   today=date.today()
+   #mood get today of self user
+   query="""select * from mood where created_by_id=:created_by_id and created_at::date=:today and type=:type"""
+   values={"created_by_id":user_id,"today":today,"type":type}
+   response=await database_fetch_all(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   row=response["message"]
+   #mood assign
+   mood=row
+   #mood not exist
+   if mood==[]:
+      query="""insert into mood (created_by_id,type,data) values (:created_by_id,:type,json_build_object('sex_o_scale',:payload))"""
+      values={"created_by_id":user_id,"type":'sex_o_scale', "payload":payload}
+      response=await database_execute(query,values)
+      if response["status"]=="false":
+         raise HTTPException(status_code=400,detail=response)
+      return response
+   #mood exist
+   #query set
+   query="""update mood set data=json_build_object('sex_o_scale',:payload) where id=:id"""
+   values={"payload":payload,"id":mood[0]["id"]}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+
+
+
 #2 mood read:self
 @router.get("/mood/read-self-last-7-days")
 async def mood_read_self_last_7_days(request:Request):
