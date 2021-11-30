e9d0fc3dd4e11b0a44b65f526b496e7cd9acdedc
Manish Mishra <mk94mishra@gmail.com>
2021-11-30 19:54:08 +0530

    blog section

--- endpoint/blog.py
+++ endpoint/blog.py
@@ -17,34 +17,48 @@ class media_type(str, Enum):
 
 
 #schema
-#1 blog
-class blog(BaseModel):
+#1 blog: period
+class period(BaseModel):
    title:str
-   data: str
+   description: str
    link_url:str
    media_type:media_type
    media_thumbnail_url:str
    media_url:str
-   type:str
-   day:str
+   day:int
+
+
+#2 blog: collection
+class collection(BaseModel):
+   title:str
+   collection: str
+   course_name:str
+   description:str
+   link_url:str
+   media_type:media_type
+   media_thumbnail_url:str
+   media_url:str
+   day:int
+
+
 
 
 #endpoint
-#1 blog create
-@router.post("/blog")
-async def blog_create(request:Request,payload:blog):
+#1 blog period create
+@router.post("/blog/period")
+async def period_create(request:Request,payload:period):
    #prework
    user_id = request.state.user_id
    payload=payload.dict()
-   
+   blog_type = 'period'
    #payload check
    if payload['title']=='' and payload['description']=='' and payload['link_url']=='' and payload['media_url']=='':
       raise HTTPException(status_code=400,detail="description,link_url,media_url:any one is needed")
 
-   description = {'description':payload['description']}
+   description = json.dumps({'description':payload['description']})
    #query set
-   query="""insert into blog (created_by_id,title,link_url,media_type,media_thumbnail_url,media_url,data,type,day) values (:created_by_id,:title,:link_url,:media_type,:media_thumbnail_url,:media_url,:description,:type,:day) returning *"""
-   values={"created_by_id":user_id,"title":payload['title'],"description":description,"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"type":payload['type'],"day":payload['day']}
+   query="""insert into blog (created_by_id,title,link_url,media_type,media_thumbnail_url,media_url,type,day,data) values (:created_by_id,:title,:link_url,:media_type,:media_thumbnail_url,:media_url,:type,:day,:data) returning *"""
+   values={"created_by_id":user_id,"title":payload['title'],"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"type":blog_type,"day":payload['day'],"data":description}
    #query run
    response=await database_execute(query,values)
    if response["status"]=="false":
@@ -53,9 +67,110 @@ async def blog_create(request:Request,payload:blog):
    return response
    
 
-# update blog
-@router.put("/blog/{blog_id}")
-async def blog_create(request:Request,payload:blog,blog_id:int):
+
+
+# blog : harmonometer create
+@router.post("/blog/harmonometer")
+async def harmonometer_create(request:Request,payload:period):
+   #prework
+   user_id = request.state.user_id
+   payload=payload.dict()
+   blog_type = 'harmonometer'
+   #payload check
+   if payload['title']=='' and payload['description']=='' and payload['link_url']=='' and payload['media_url']=='':
+      raise HTTPException(status_code=400,detail="description,link_url,media_url:any one is needed")
+
+   description = json.dumps({'description':payload['description']})
+   #query set
+   query="""insert into blog (created_by_id,title,link_url,media_type,media_thumbnail_url,media_url,type,day,data) values (:created_by_id,:title,:link_url,:media_type,:media_thumbnail_url,:media_url,:type,:day,:data) returning *"""
+   values={"created_by_id":user_id,"title":payload['title'],"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"type":blog_type,"day":payload['day'],"data":description}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+   
+
+   
+
+# blog : moodometer create
+@router.post("/blog/moodometer")
+async def moodometer_create(request:Request,payload:period):
+   #prework
+   user_id = request.state.user_id
+   payload=payload.dict()
+   blog_type = 'moodometer'   
+   #payload check
+   if payload['title']=='' and payload['description']=='' and payload['link_url']=='' and payload['media_url']=='':
+      raise HTTPException(status_code=400,detail="description,link_url,media_url:any one is needed")
+
+   description = json.dumps({'description':payload['description']})
+   #query set
+   query="""insert into blog (created_by_id,title,link_url,media_type,media_thumbnail_url,media_url,type,day,data) values (:created_by_id,:title,:link_url,:media_type,:media_thumbnail_url,:media_url,:type,:day,:data) returning *"""
+   values={"created_by_id":user_id,"title":payload['title'],"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"type":blog_type,"day":payload['day'],"data":description}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+   
+
+   
+
+# blog : sexoscale create
+@router.post("/blog/sexoscale")
+async def sexoscale_create(request:Request,payload:period):
+   #prework
+   user_id = request.state.user_id
+   payload=payload.dict()
+   blog_type = 'sex_o_scale'
+   #payload check
+   if payload['title']=='' and payload['description']=='' and payload['link_url']=='' and payload['media_url']=='':
+      raise HTTPException(status_code=400,detail="description,link_url,media_url:any one is needed")
+
+   description = json.dumps({'description':payload['description']})
+   #query set
+   query="""insert into blog (created_by_id,title,link_url,media_type,media_thumbnail_url,media_url,type,day,data) values (:created_by_id,:title,:link_url,:media_type,:media_thumbnail_url,:media_url,:type,:day,:data) returning *"""
+   values={"created_by_id":user_id,"title":payload['title'],"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"type":blog_type,"day":payload['day'],"data":description}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+   
+
+
+
+#2 blog collection create
+@router.post("/blog/collection")
+async def collection_create(request:Request,payload:collection):
+   #prework
+   user_id = request.state.user_id
+   payload=payload.dict()
+   blog_type = 'collection'
+   #payload check
+   if payload['title']=='' and payload['collection']=='' and payload['link_url']=='' and payload['media_url']=='':
+      raise HTTPException(status_code=400,detail="collection,link_url,media_url:any one is needed")
+
+   json_data = json.dumps({'collection':payload['collection'],'description':payload['description'],'course_name':payload['course_name']})
+   #query set
+   query="""insert into blog (created_by_id,title,link_url,media_type,media_thumbnail_url,media_url,type,day,data) values (:created_by_id,:title,:link_url,:media_type,:media_thumbnail_url,:media_url,:type,:day,:data) returning *"""
+   values={"created_by_id":user_id,"title":payload['title'],"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"type":blog_type,"day":payload['day'],"data":json_data}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+   
+
+
+# update blog: period
+@router.put("/blog/period/{blog_id}")
+async def blog_period_update(request:Request,payload:period,blog_id:int):
    #prework
    user_id = request.state.user_id
    payload=payload.dict()
@@ -64,10 +179,10 @@ async def blog_create(request:Request,payload:blog,blog_id:int):
    if payload['title']=='' and payload['description']=='' and payload['link_url']=='' and payload['media_url']=='':
       raise HTTPException(status_code=400,detail="title,description,link_url,media_url:any one is needed")
 
-   description = {'description':payload['description']}
+   description = json.dumps({'description':payload['description']})
    #query set
    query="""update blog set title=:title, link_url=:link_url,media_type=:media_type,media_thumbnail_url=:media_thumbnail_url,media_url=:media_url,data=:description,day=:day where id=:blog_id returning *"""
-   values={"created_by_id":user_id,"title":payload['title'],"description":description,"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"type":payload['type'],"day":payload['day'],"blog_id":blog_id}
+   values={"title":payload['title'],"description":description,"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"day":payload['day'],"blog_id":blog_id}
    #query run
    response=await database_execute(query,values)
    if response["status"]=="false":
@@ -75,6 +190,102 @@ async def blog_create(request:Request,payload:blog,blog_id:int):
    #finally
    return response
 
+
+# update blog: harmonometer
+@router.put("/blog/harmonometer/{blog_id}")
+async def blog_harmonometer_update(request:Request,payload:period,blog_id:int):
+   #prework
+   user_id = request.state.user_id
+   payload=payload.dict()
+   
+   #payload check
+   if payload['title']=='' and payload['description']=='' and payload['link_url']=='' and payload['media_url']=='':
+      raise HTTPException(status_code=400,detail="title,description,link_url,media_url:any one is needed")
+
+   description = json.dumps({'description':payload['description']})
+   #query set
+   query="""update blog set title=:title, link_url=:link_url,media_type=:media_type,media_thumbnail_url=:media_thumbnail_url,media_url=:media_url,data=:description,day=:day where id=:blog_id returning *"""
+   values={"title":payload['title'],"description":description,"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"day":payload['day'],"blog_id":blog_id}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+
+
+
+
+# update blog: moodometer
+@router.put("/blog/moodometer/{blog_id}")
+async def blog_moodometer_update(request:Request,payload:period,blog_id:int):
+   #prework
+   user_id = request.state.user_id
+   payload=payload.dict()
+   
+   #payload check
+   if payload['title']=='' and payload['description']=='' and payload['link_url']=='' and payload['media_url']=='':
+      raise HTTPException(status_code=400,detail="title,description,link_url,media_url:any one is needed")
+
+   description = json.dumps({'description':payload['description']})
+   #query set
+   query="""update blog set title=:title, link_url=:link_url,media_type=:media_type,media_thumbnail_url=:media_thumbnail_url,media_url=:media_url,data=:description,day=:day where id=:blog_id returning *"""
+   values={"title":payload['title'],"description":description,"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"day":payload['day'],"blog_id":blog_id}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+
+
+
+
+
+# update blog: sexoscale
+@router.put("/blog/sexoscale/{blog_id}")
+async def blog_sexoscale_update(request:Request,payload:period,blog_id:int):
+   #prework
+   user_id = request.state.user_id
+   payload=payload.dict()
+   
+   #payload check
+   if payload['title']=='' and payload['description']=='' and payload['link_url']=='' and payload['media_url']=='':
+      raise HTTPException(status_code=400,detail="title,description,link_url,media_url:any one is needed")
+
+   description = json.dumps({'description':payload['description']})
+   #query set
+   query="""update blog set title=:title, link_url=:link_url,media_type=:media_type,media_thumbnail_url=:media_thumbnail_url,media_url=:media_url,data=:description,day=:day where id=:blog_id returning *"""
+   values={"title":payload['title'],"description":description,"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"day":payload['day'],"blog_id":blog_id}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+
+
+# update blog: collection
+@router.put("/blog/collection/{blog_id}")
+async def blog_collection_update(request:Request,payload:collection,blog_id:int):
+   #prework
+   user_id = request.state.user_id
+   payload=payload.dict()
+   #payload check
+   if payload['title']=='' and payload['collection']=='' and payload['link_url']=='' and payload['media_url']=='':
+      raise HTTPException(status_code=400,detail="collection,link_url,media_url:any one is needed")
+
+   json_data = json.dumps({'collection':payload['collection'],'description':payload['description'],'course_name':payload['course_name']})
+   #query set
+   query="""update blog set title=:title,link_url=:link_url,media_type=:media_type,media_thumbnail_url=:media_thumbnail_url,media_url=:media_url,day=:day,data=:data where id=:blog_id returning *"""
+   values={"title":payload['title'],"link_url":payload['link_url'],"media_type":payload['media_type'],"media_thumbnail_url":payload['media_thumbnail_url'],"media_url":payload['media_url'],"day":payload['day'],"data":json_data, 'blog_id':blog_id}
+   #query run
+   response=await database_execute(query,values)
+   if response["status"]=="false":
+      raise HTTPException(status_code=400,detail=response)
+   #finally
+   return response
+   
 
 
 #3 blog read:by all
--- setting.py
+++ setting.py
@@ -7,7 +7,7 @@ max_size=10
 
 #2 database object url
 from databases import Database
-database = Database('postgresql://{}:{}@{}:{}/{}'.format(config['db_user'],config['db_password'],config['db_host'],config['db_port'],config['db_name']),max_size=70)
+database = Database('postgresql://{}:{}@{}:{}/{}'.format(config['db_user'],config['db_password'],config['db_host'],config['db_port'],config['db_name']),max_size=70,min_size=5)
 
 #3 public endpoints
 endpoint_public=[("GET",""),("GET","docs"),("GET","redoc"),("GET","openapi.json"),("POST","user/login-admin"),("POST","user/login-non-admin"),("POST","user/signup-normal")]
