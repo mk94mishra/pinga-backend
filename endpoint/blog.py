622906ec0e714a05f825c44fbb0dcddf2f4f9f10
Manish Mishra <mk94mishra@gmail.com>
2021-11-30 23:31:44 +0530

    blog

--- endpoint/blog.py
+++ endpoint/blog.py
@@ -313,10 +313,11 @@ async def blog_read(request:Request,offset:int):
 async def blog_read_collection(request:Request,collection:str,offset:int):
    #prework
    user_id = request.state.user_id
-   collection = "'%"+collection+"%'::jsonb"
+   collection = "'%"+collection+"%'"
    #query set
-   query="""select * from blog where data->'collection' like :collection limit 10 offset :offset;"""
+   query="""select * from blog where data->>'collection' like :collection limit 10 offset :offset;"""
    values={"collection":collection,"offset":offset}
+   print(query)
    #query run
    response=await database_fetch_all(query,values)
    if response["status"]=="false":
