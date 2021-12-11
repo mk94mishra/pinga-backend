a1c81fc4f18abc422969f2d31f1e84e6eee9dc26
Manish Mishra <mk94mishra@gmail.com>
2021-12-11 21:09:47 +0530

    google login

--- setting.py
+++ setting.py
@@ -10,5 +10,5 @@ from databases import Database
 database = Database('postgresql://{}:{}@{}:{}/{}'.format(config['db_user'],config['db_password'],config['db_host'],config['db_port'],config['db_name']),min_size=5,max_size=70)
 
 #3 public endpoints
-endpoint_public=[("GET",""),("GET","docs"),("GET","redoc"),("GET","openapi.json"),("POST","user/login-admin"),("POST","user/login-non-admin"),("POST","user/signup-normal")]
+endpoint_public=[("GET",""),("GET","docs"),("GET","redoc"),("GET","openapi.json"),("POST","user/login-admin"),("POST","user/login-non-admin"),("POST","user/signup-normal"),("POST","login-non-admin-google-auth"),("POST","user/signup-google")]
 
