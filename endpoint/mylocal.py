6e998b6e93bbf49a0162e17390dde06697cb63d7
Manish Mishra <mk94mishra@gmail.com>
2022-01-09 22:11:23 +0530

    email

--- endpoint/mylocal.py
+++ endpoint/mylocal.py
@@ -61,3 +61,19 @@ sql_data = [
 
 #    return "rest"
 
+
+
+
+@router.get("/mylocal")
+async def mylocal_create(request:Request):
+   import emails
+   message = emails.html(html="<p>Hi!<br>Here is your receipt...",
+                           subject="Your receipt No. 567098123",
+                           mail_from=('Some Store', 'manish@pingaweb.com'))
+
+
+   r = message.send(to='mk94mishra@gmail.com', smtp={'host': 'pingaweb.com', 'timeout': 5})
+   assert r.status_code == 250
+
+   return "rest"
+
