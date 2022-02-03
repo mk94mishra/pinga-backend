ff3064ac51a67defbbe5b289b9383409d12b4df3
Manish Mishra <mk94mishra@gmail.com>
2022-02-03 23:11:42 +0530

    storage

--- awsimage.py
+++ awsimage.py
@@ -1,17 +1,24 @@
 # s3 upload
 import boto3
 import uuid
+import requests
 def get_presigned_url(filename_with_extension,user_id):
     #unique s3 key
     key=str(uuid.uuid4())+"_"+str(user_id)+"_"+filename_with_extension
     #s3 client
     s3_client=boto3.client('s3',region_name="ap-south-1",aws_access_key_id="AKIA22OPN45JSL7EMXME",aws_secret_access_key="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")
     #generate presigned post url
-    response=s3_client.generate_presigned_post(Bucket="pingaweb-aps1",Key=key,ExpiresIn=1000)
+    response=s3_client.generate_presigned_post(Bucket="pinga-storage",Key=key,ExpiresIn=1000)
+    print(response)
+    #Upload file to S3 using presigned URL
+    files = { 'file': open('myimage.jpeg', 'rb')}
+    r = requests.post(response['url'], data=response['fields'], files=files)
+    print(r)
+
     return response
 
 
-print(get_presigned_url("manis.jpeg","1"))
+get_presigned_url("myimage.jpeg",1)
 # aws_access_key_id="AKIA22OPN45JSL7EMXME"
 # aws_secret_access_key="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"
 # aws_service="s3"
