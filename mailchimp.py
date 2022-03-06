13804ea10be8cba6ce8dd72d7656d0e293d225d1
Manish Mishra <mk94mishra@gmail.com>
2022-03-06 20:24:11 +0530

    mailchimp

--- mailchimp.py
+++ mailchimp.py
@@ -1,12 +1,12 @@
-import mailchimp_transactional as MailchimpTransactional
-from mailchimp_transactional.api_client import ApiClientError
+# import mailchimp_transactional as MailchimpTransactional
+# from mailchimp_transactional.api_client import ApiClientError
 
-def run():
-  try:
-    mailchimp = MailchimpTransactional.Client('877cb4df376f85992cb453bfe707bcb1-us20')
-    response = mailchimp.users.ping()
-    print('API called successfully: {}'.format(response))
-  except ApiClientError as error:
-    print('An exception occurred: {}'.format(error.text))
+# def run():
+#   try:
+#     mailchimp = MailchimpTransactional.Client('877cb4df376f85992cb453bfe707bcb1-us20')
+#     response = mailchimp.users.ping()
+#     print('API called successfully: {}'.format(response))
+#   except ApiClientError as error:
+#     print('An exception occurred: {}'.format(error.text))
 
-run()
+# run()
