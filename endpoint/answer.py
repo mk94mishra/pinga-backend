2968577cbb94c592f92e62c9363c05d7bd0513b6
Nitin Virmani <Nitin-Virmani@Smritis-MacBook-Air.local>
2024-02-13 02:13:19 +0530

    update get answer query to fetch only the latest answers submitted by a user for a form

--- endpoint/answer.py
+++ endpoint/answer.py
@@ -61,18 +61,37 @@ async def answer_read(request:Request,user_id:int,form_id:int):
 
    #query set
    query="""
-   with
-   q as (select * from question),
-   o as (select * from "option")
-
-   select 
-   a.*,
-   q.id as question_id, q.title as question_title, q.media_type as question_media_type, q.media_url as question_media_url, q.media_thumbnail_url as question_media_thumbnail_url,
-   o.title as option_title,o.media_type as option_media_type, o.media_url as option_media_url, o.media_thumbnail_url as option_media_thumbnail_url, o.weightage as option_weightage 
-   from answer as a 
-   left join o on o.id=a.option_id
-   left join q on q.id=o.question_id
-   where q.form_id=:form_id and a.created_by_id=:user_id and a.flag is null
+      WITH latest_answers AS (
+         SELECT 
+            a.*,
+            q.id AS question_id,
+            q.title AS question_title,
+            q.media_type AS question_media_type,
+            q.media_url AS question_media_url,
+            q.media_thumbnail_url AS question_media_thumbnail_url,
+            o.title AS option_title,
+            o.media_type AS option_media_type,
+            o.media_url AS option_media_url,
+            o.media_thumbnail_url AS option_media_thumbnail_url,
+            o.weightage AS option_weightage,
+            ROW_NUMBER() OVER (PARTITION BY a.created_by_id, q.form_id, a.option_id ORDER BY a.created_at DESC) AS rn
+         FROM 
+            answer AS a
+         LEFT JOIN 
+            option AS o ON o.id = a.option_id
+         LEFT JOIN 
+            question AS q ON q.id = o.question_id
+         WHERE 
+            q.form_id = :form_id 
+            AND a.created_by_id = :user_id 
+            AND a.flag IS NULL
+      )
+      SELECT 
+         *
+      FROM 
+         latest_answers
+      WHERE 
+         rn = 1;
    """
    values={"form_id":form_id, "user_id":user_id}
    #query run
